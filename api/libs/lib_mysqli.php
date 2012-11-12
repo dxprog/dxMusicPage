b2f4cef2a521ca2d92e8ca68a67784b60796bd4a
Matt Hackmann <matt.hackmann@gmail.com>
2012-11-11 20:17:47 -0600

    Code cleanup

--- api/libs/lib_mysqli.php
+++ api/libs/lib_mysqli.php
@@ -73,7 +73,9 @@ function db_Query ($query, $errSuppress = false)
 	if (!($result = $_dbconn->query($query)) && !$errSuppress) {
 		raiseError (DBERR_BAD_QUERY, $_dberr[DBERR_BAD_QUERY].$_dbconn->error."\n".$query);
 	}
-		
+	
+	$out = new stdClass;
+	
 	// Determine the output based upon the command used
 	switch (strtolower (substr ($query, 0, 6))) {
 		case 'select':
